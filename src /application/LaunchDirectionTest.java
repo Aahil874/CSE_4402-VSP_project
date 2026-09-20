b77a1f8004c13641443dc124bdc14de9654097ec
aahilzaman <aahilzaman@gmail.com>
2026-09-20 14:40:56 +0600

    Replace old project files with working JavaFX version

--- src /application/LaunchDirectionTest.java
+++
@@ -1,7 +0,0 @@
-package application;
-public final class LaunchDirectionTest {
- public static void main(String[] args){
-  for(double[] d:new double[][]{{-1,0},{0,-1},{1,0},{0,1}}){LiveBallController c=new LiveBallController();c.launchBattedBall(560,548,.8,HitResult.SINGLE,d[0],d[1]);if(Math.hypot(c.ball().velocityX(),c.ball().velocityY())<1)throw new AssertionError("zero launch");}
-  System.out.println("LAUNCH_DIRECTIONS=PASS");
- }
-}
--- src /module-info.java
+++
@@ -1,8 +0,0 @@
-module final_homerun {
-	requires javafx.controls;
-	requires java.sql;
-	requires javafx.fxml;
-	requires mysql.connector.java;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
